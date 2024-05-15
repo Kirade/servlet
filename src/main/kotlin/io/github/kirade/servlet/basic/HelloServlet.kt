3c58c17e5f53eb24b125b0403878f7b51c4fea86
JaeyoungHeo <jay.jaeyoung@gmail.com>
2024-05-15 22:50:54 +0900

    feat: hello servlet

---
+++ src/main/kotlin/io/github/kirade/servlet/basic/HelloServlet.kt
@@ -0,0 +1,23 @@
+package io.github.kirade.servlet.basic
+
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+
+@WebServlet(name = "Hello", urlPatterns = ["/hello"])
+class HelloServlet: HttpServlet() {
+
+    override fun service(request: HttpServletRequest?, response: HttpServletResponse?) {
+        println("HelloServlet.service")
+        println(request)
+        println(response)
+
+        val username = request?.getParameter("username")
+        println(username)
+
+        response?.contentType = "text/plain"
+        response?.characterEncoding = "utf-8"
+        response?.writer?.write("hello $username")
+    }
+}
